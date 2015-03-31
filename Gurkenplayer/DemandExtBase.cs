cf7a9e1fe3591fcd42cf8e84007f841b6762cdb1
Gurkenschreck <mariogiugno@gmx.de>
2015-03-31 12:55:48 +0200

    Added notes.txt and DemandExtBase.cs

---
+++ Gurkenplayer/DemandExtBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICities;
+using UnityEngine;
+using ColossalFramework;
+
+namespace Gurkenplayer
+{
+    class DemandExtBase : DemandExtensionBase
+    {
+
+    }
+}
